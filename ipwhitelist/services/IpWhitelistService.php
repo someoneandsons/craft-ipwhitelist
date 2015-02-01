1c34e99fcf3a5bb37e0fdd3b8fab9a0eef89f8a0
Sam Hibberd <sam@fruitstudios.co.uk>
2015-02-01 18:21:07 +0000

    Exclude Paths
    Added the ability to exclude paths from the ip restrictions.

--- ipwhitelist/services/IpWhitelistService.php
+++ ipwhitelist/services/IpWhitelistService.php
@@ -3,23 +3,53 @@ namespace Craft;
 
 class IpWhitelistService extends BaseApplicationComponent
 {
+	protected $plugin;
+
+    public function __construct()
+    {    
+        $this->plugin = craft()->plugins->getPlugin('ipWhitelist');
+    }	
+
     public function _checkCode($code = '') {
-	    $settings = craft()->plugins->getPlugin('ipWhitelist')->getSettings();
+	    $settings = $this->plugin->getSettings();
 		$codeSetting = $settings->code;
 		
 		if($code === $codeSetting && !$this->_checkIP(craft()->request->getIpAddress())) {
 			$settings->setAttribute('whitelist', $settings->whitelist."\n".craft()->request->getIpAddress());
 			
-			craft()->plugins->savePluginSettings(craft()->plugins->getPlugin('ipWhitelist'), $settings);
+			craft()->plugins->savePluginSettings($this->plugin, $settings);
 		}
     }
     
     public function _checkIP($ip = '127.0.0.1') {		
-		$settings = craft()->plugins->getPlugin('ipWhitelist')->getSettings();
+		$settings = $this->plugin->getSettings();
 		$whitelistSetting = $settings->whitelist;
 		
 		$whitelist = array_map('trim', explode("\n", $whitelistSetting));
 		
 		return in_array($ip, $whitelist);
     }
+
+    public function _isExcluded() {	
+		$settings = $this->plugin->getSettings();
+		$requestPath = craft()->request->getPath();
+		
+		$exclude = array_map('trim', explode('\n', $settings->exclude));
+
+		$excluded = false;
+		if(is_array($exclude))
+		{
+			foreach($exclude as $path)
+			{
+				if(strrpos($requestPath, $path, -strlen($requestPath)) !== FALSE)
+				{
+					$excluded = true;
+					break;
+				}
+
+			}
+		}
+		return $excluded;
+    }
+    
 }
